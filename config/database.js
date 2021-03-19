bc7e2ba9decee17cba835913e6eec00ffaaca176
David Freitas <davidsonmfreitas@gmail.com>
2021-03-19 00:43:33 -0300

    changes to docker

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgresql-rigid-61625',
+        client: 'postgres',
         host: env('DATABASE_HOST', 'ec2-52-21-252-142.compute-1.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'dbolbmgdj4v8am'),
@@ -12,7 +12,7 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', '4ad0e3def80c84c540814de6e898222b722e29606f08d0161586cb47512eb252'),
         ssl: env.bool('DATABASE_SSL', false),
       },
-      options: { }
+      options: {}
     },
   },
 });
