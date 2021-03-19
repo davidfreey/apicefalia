fe8d01af28c4208d2854269c61283dc5d83bb303
David Freitas <davidsonmfreitas@gmail.com>
2021-03-19 01:22:06 -0300

    changes to heroku

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'ec2-52-21-252-142.compute-1.amazonaws.com'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'dbolbmgdj4v8am'),
-        username: env('DATABASE_USERNAME', 'qgvwitodqnkshd'),
-        password: env('DATABASE_PASSWORD', '4ad0e3def80c84c540814de6e898222b722e29606f08d0161586cb47512eb252'),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'docker'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
